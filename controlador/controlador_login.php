70736e1eab925c934e5a93e94ea039449725e11b
GaruCH <gabrielch1805@gmail.com>
2024-04-07 01:10:56 -0600

    pequeñas configuraciones

--- controlador/controlador_login.php
+++ controlador/controlador_login.php
@@ -1,16 +1,19 @@
 <?php
-// Verifica si el formulario fue enviado
+// Iniciar o reanudar una sesión
+session_start();
+
+// Verificar si el formulario fue enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btningresar"])) {
-    // Verifica si los campos de usuario y contraseña no están vacíos
+    // Verificar si los campos de usuario y contraseña no están vacíos
     if (!empty($_POST["usuario"]) && !empty($_POST["password"])) {
-        // Incluye el archivo de conexión a la base de datos
+        // Incluir el archivo de conexión a la base de datos
         include "../modelos/BD/conexion.php";
 
-        // Limpia los datos del usuario y la contraseña para prevenir inyección de SQL
+        // Limpiar los datos del usuario y la contraseña para prevenir la inyección de SQL
         $usuario = htmlspecialchars($_POST["usuario"]);
         $password = htmlspecialchars($_POST["password"]);
 
-        // Prepara la consulta utilizando sentencias preparadas para prevenir la inyección de SQL
+        // Preparar la consulta utilizando sentencias preparadas para prevenir la inyección de SQL
         $sql = "SELECT * FROM usuarios WHERE Usuario = :usuario AND Contrasena = :password";
         $stmt = $conexion->prepare($sql);
         $stmt->bindParam(":usuario", $usuario);
@@ -18,11 +21,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btningresar"])) {
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // Verifica si se encontró un usuario con las credenciales proporcionadas
+        // Verificar si se encontró un usuario con las credenciales proporcionadas
         if ($result) {
-            // Inicio de sesión exitoso, redirige al usuario a la página de inicio
+            // Inicio de sesión exitoso, establecer variables de sesión
+            $_SESSION["usuario"] = $usuario;
+            $_SESSION["loggedin"] = true;
+
+            // Redirigir al usuario a la página de inicio
             header("Location: index.html");
-            exit(); // Importante para prevenir que el script siga ejecutándose
+            exit();
         } else {
             // No se encontró ningún usuario con las credenciales proporcionadas
             echo "<div class='alert alert-danger'>Acceso denegado</div>";
