f05a74d688a3e160a8748d1534fc4187b41f34ec
GaruCH <146020588+GaruCH@users.noreply.github.com>
2024-04-16 15:33:57 -0600

    cambios minimos

--- views/Receta.php
+++ views/Receta.php
@@ -105,7 +105,7 @@ if (!isset($_SESSION["ID_Usuario"])) {
                     echo "<td>" . $dato['Receta'] . "</td>";
                     echo "<td>";
                     echo "<div class='eliminar-container'>";
-                    echo "<button onclick=\"window.location.href='index.php?c=DetallesP&a=index&id=" . $dato['ID_Paciente'] . "'\">Editar</button>";
+                    echo "<button onclick=\"window.location.href='index.php?c=DetallesR&a=index&id=" . $dato['ID_Paciente'] . "'\">Editar</button>";
                     echo "<form action='index.php?c=Pacientes&a=eliminarPaciente' method='post'>";
                     echo "<input type='hidden' name='id_paciente' value='" . $dato['ID_Paciente'] . "'>";
                     echo "<button type='submit' onclick=\"return confirm('¿Estás seguro de que deseas eliminar este paciente?')\">Eliminar</button>";
