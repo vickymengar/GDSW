b3898a6b659f6a7a69491ad753a7ad15259d449a
GaruCH <gabrielch1805@gmail.com>
2024-04-15 22:26:38 -0600

    Carga de datos a tabla citas funciona

--- controllers/Citas.php
+++ controllers/Citas.php
@@ -1,14 +1,13 @@
 <?php
-// PanelController.php
-
 class CitasController {
+
     public function index() {
-        // Aquí puedes realizar cualquier lógica necesaria para cargar datos o realizar acciones antes de mostrar la vista del panel
-        
-        // Por ejemplo, podrías cargar datos de algún modelo para mostrar en la vista
-        
-        // Luego, incluyes la vista del panel
-        require_once 'views/citas.php';
+
+        require_once "models/citas/CitasModel.php";
+        $citas_model = new Citas_model();
+        $citas["citas"] = $citas_model->get_citas();
+
+        require_once "views/citas.php";
     }
 }
 ?>
